2471fcc76ced8c1ddf24ab4eced87354d60d0759
cenda08 <denisa.slegrova@gmail.com>
2026-07-22 22:51:25 +0200

    pohyb hada nefunkční
    zvětšenej pohyb hada, ale konec nefunkční

--- Koala Game/Assets/Scripts/Snake_Script.cs
+++ Koala Game/Assets/Scripts/Snake_Script.cs
@@ -6,11 +6,14 @@ public class Snake_Script : MonoBehaviour
     public GameObject Snake;
     public float SnakeSpeed;
     public float TargetDown;
+    public float TargetDown2;
     public float TargetUp;
+    public float TargetUp2;
     public float TargetLeft;
     public float TargetRight;
+    public float TargetRight2;
     //private bool SnakeIsMoving = false;
-   
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -59,9 +62,38 @@ public class Snake_Script : MonoBehaviour
             Debug.Log("pohybnahoru");
             yield return null;
         }
+
+
+        while (transform.position.x <= TargetRight2)
+        {
+            transform.Translate(Vector3.right * SnakeSpeed * Time.deltaTime * 1/2);
+            Debug.Log("pohyb doprava");
+            yield return null;
+        }
+
+        while (transform.position.y >= TargetDown2)
+        {
+            transform.Translate(Vector3.down * SnakeSpeed * Time.deltaTime);
+            Debug.Log("pohyb dolů");
+            yield return null;
+
+        }
+        while (transform.position.y >= TargetLeft)
+        {
+            transform.Translate(Vector3.left * SnakeSpeed * Time.deltaTime);
+            Debug.Log("pohyb dolů");
+            yield return null;
+
+        }
+        while (transform.position.y <= TargetUp2)
+        {
+            transform.Translate(Vector3.down * SnakeSpeed * Time.deltaTime);
+            Debug.Log("pohybnahoru");
+            yield return null;
+        }
+    }
     }
 
 
    
-}
 
