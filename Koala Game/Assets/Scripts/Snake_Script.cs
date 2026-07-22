74207bda6cd94a14d8948358876b4ddcbd2cbd93
cenda08 <denisa.slegrova@gmail.com>
2026-07-22 11:11:04 +0200

    SnakeScript, nutno opravit

---
+++ Koala Game/Assets/Scripts/Snake_Script.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+public class Snake_Script : MonoBehaviour
+{
+
+    public GameObject Snake;
+    public float SnakeSpeed;
+    private float PreviousSnakeSpeed;
+    public float TargetDown;
+    public float TargetUp;
+    public float TargetLeft;
+    public float TargetRight;
+   
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SnakeMovement();
+    }
+
+    void SnakeMovement()
+    {
+        transform.Translate(Vector3.down * SnakeSpeed * Time.deltaTime);
+        if (transform.position.y <= TargetDown) SnakeSpeed = 0;
+        RestoreSpeed();
+        transform.Translate(Vector3.right * SnakeSpeed * Time.deltaTime);
+        if (transform.position.x >= TargetRight) SnakeSpeed = 0;
+        RestoreSpeed();
+
+    }
+
+    public void RestoreSpeed()
+    {
+        SnakeSpeed = PreviousSnakeSpeed; 
+    }
+}
