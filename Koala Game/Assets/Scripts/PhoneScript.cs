7dcdaf1c91b67ef32047a8eac601ea12f4db3727
cenda08 <denisa.slegrova@gmail.com>
2026-07-24 15:24:00 +0200

    hintscreen

--- Koala Game/Assets/Scripts/PhoneScript.cs
+++ Koala Game/Assets/Scripts/PhoneScript.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using System.Collections;
 
 public class PhoneScript : MonoBehaviour
 {
+
+    public LogicScript logic;
+    public GameObject HintScreen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        void OnMouseDown()
+        {
+
+            HintScreen.SetActive(true);
+        }
     }
 }
