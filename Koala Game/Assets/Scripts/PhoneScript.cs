864f816aac9389de347321d65e8938bdcbbc4b06
cenda08 <denisa.slegrova@gmail.com>
2026-07-24 15:48:39 +0200

    aaa

--- Koala Game/Assets/Scripts/PhoneScript.cs
+++ Koala Game/Assets/Scripts/PhoneScript.cs
@@ -6,6 +6,9 @@ public class PhoneScript : MonoBehaviour
     public GameObject Phone;
     public LogicScript logic;
     public GameObject HintScreen;
+    bool CanClickHint = true;
+    public GameObject Accept;
+    public GameObject Decline;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,10 +24,23 @@ public class PhoneScript : MonoBehaviour
 
     IEnumerator OnMouseDown()
     {
-
+        if (CanClickHint) 
+    { 
         HintScreen.SetActive(true);
         yield return new WaitForSeconds(7);
         HintScreen.SetActive(false);
         yield return null;
+            
+    }
+
+}
+
+
+    IEnumerator PhoneRings()
+    {
+        yield return new WaitForSeconds(30);
+        Accept.SetActive(true);
+        Decline.SetActive(true);
+
     }
 }
